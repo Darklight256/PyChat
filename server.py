1167f4fc9147f5d725552f86e7f5a7ed58f19c39
Parsa Haqiqat <59744992+parsa-haqiqat@users.noreply.github.com>
2020-06-09 12:52:18 +0430

    Add files via upload

--- server.py
+++ server.py
@@ -6,16 +6,18 @@ from mysql.connector import Error
 
 ##############################DATABASE CONNECTION
 connection = mysql.connector.connect(host='localhost',
-                                    database='user_of_PyChat',
-                                    user='root',
-                                    password='root',
-                                    port=8889)
+                                     database='user_of_PyChat',
+                                     user='root',
+                                     password='root',
+                                     port=8889)
 print(connection)
 mycursor = connection.cursor()
+
+
 #################################################
 
 ##############################GET INFO CLASS
-#1.fname 2.lname 3.username 4.uid 5.random status
+# 1.fname 2.lname 3.username 4.uid 5.random status
 class usr_info:
     def __init__(self, fname, lname, name, uID, stts):
         self.Fname(fname)
@@ -23,6 +25,8 @@ class usr_info:
         self.username(name)
         self.UID(uID)
         self.rand_stts(stts)
+
+
 ############################################
 
 ##############################GET INFO FUNCTION
@@ -42,7 +46,7 @@ def all_info_uid(uID):
     sql = "SELECT uid FROM `random` WHERE uid = " + uID
     mycursor.execute(sql)
     res_size = mycursor.fetchall()
-    
+
     rndstts = 0
 
     if len(res_size) > 0:
@@ -50,6 +54,8 @@ def all_info_uid(uID):
 
     result = usr_info(usrfname, usrlname, usrname, str(uID), str(rndstts))
     return result
+
+
 ###############################################
 
 ##############################GET INFO FUNCTION
@@ -77,6 +83,8 @@ def all_info_usrname(name):
 
     result = usr_info(usrfname, usrlname, name, str(UID), str(rndstts))
     return result
+
+
 ###############################################
 
 ##############################DATABASE ERASE
@@ -84,30 +92,35 @@ def reset_database_user(name):
     sql = "DELETE FROM `usr` WHERE username = " + name
     mycursor.execute(sql)
     connection.commit()
+
+
 #############################################
 
 ##############################SET NEW PASSWORD
-def set_new_password(name, fname, lname, pwd):
+def set_new_password(name, fname, lname, hsh):
     sql = "SELECT MAX(uid) FROM `usr`"
     mycursor.execute(sql)
     UID = mycursor.fetchall()[0] + 1
 
-    hsh = hashlib.pbkdf2_hmac('sha256', pwd, b'salt', 100000)
-
-    sql = "INSERT INTO `usr` (username, password, real_pass, uid, fName, lName) VALUES ("+name+", "+hsh+", " + pwd + " , " + str(UID) + ", " + fname + ", " + lname + ")";
+    sql = "INSERT INTO `usr` (username, password, uid, fName, lName) VALUES (" + name + ", " + hsh + ", " + str(
+        UID) + ", " + fname + ", " + lname + ")";
     mycursor.execute(sql)
     connection.commit()
+
+
 ##############################################
 
 ##############################VALID USERNAME FOR REGISTERATION
-#in client -> fname & lname -> username
-def valid_username_reg(name, fname, lname):
+# in client -> fname & lname -> username
+def valid_username_reg(name):
     sql = "SELECT username FROM `usr` WHERE username = " + name
     mycursor.execute(sql)
     myresult = mycursor.fetchall()
     if len(myresult) > 0:
         return False
     return True
+
+
 ###############################################################
 
 ##############################VALID USERNAME
@@ -118,7 +131,21 @@ def valid_username(name):
     if len(myresult) > 0:
         return True
     return False
+
+
 ##########################################
+
+##############################CHANGE USERNAME
+def change_username (UID, new_name) :
+    if valid_username(new_name) == False :
+        sql = "UPDATE `usr` SET username = " + new_name + " WHERE uid = " + UID
+        mycursor.execute(sql)
+        connection.commit()
+        return True
+    return False
+
+
+#############################################
 
 #############################CHECK PASSWORD
 def check_password(name, hsh):
@@ -128,19 +155,20 @@ def check_password(name, hsh):
     if myresult == hsh:
         return True
     return False
+
+
 ###########################################
 
 #############################CHANGE PASSWORD
-def change_password(name, hsh, newhsh, newpwd):
+def change_password(name, hsh, newhsh):
     if check_password(name, hsh):
         sql = "UPDATE `usr` SET password = " + newhsh + " WHERE username = " + name
         mycursor.execute(sql)
         connection.commit()
-        sql = "UPDATE `usr` SET real_pass = " + newpwd + " WHERE username = " + name
-        mycursor.execute(sql)
-        connection.commit()
         return True
     return False
+
+
 ############################################
 
 #############################DELETE ACCOUNT
@@ -149,6 +177,8 @@ def dlt_account(name, hsh):
         reset_database_user(name)
         return True
     return False
+
+
 ############################################
 
 #############################INSERT ANSWERS
@@ -156,6 +186,8 @@ def secure_question(name, ans1, ans2, ans3):
     sql = "INSERT INTO `question` (username, answer1, answer2, answer3) VALUES (" + name + ", " + ans1 + ", " + ans2 + ", " + ans3 + ")";
     mycursor.execute(sql)
     connection.commit()
+
+
 ###########################################
 
 #############################CHECK ANSWER OF QUESTION
@@ -166,6 +198,8 @@ def check_security_answer(name, ans1, ans2, ans3):
     if myres[0] == ans1 and myres[1] == ans2 and myres[2] == ans3:
         return True
     return False
+
+
 #####################################################
 
 #############################CHANGE SECURITY QUESTIONS ANSWERS
@@ -186,6 +220,8 @@ def change_ans(name, qst, ans):
         connection.commit()
         return True
     return False
+
+
 ##############################################################
 
 #############################ADD FRIEND
@@ -204,6 +240,8 @@ def add_friend(name, new_frnd):
     mycursor.execute(sql)
     connection.commit()
     return True
+
+
 #######################################
 
 #############################DELETE FRIEND
@@ -217,6 +255,8 @@ def del_friend(name, del_frnd):
     mycursor.execute(sql)
     connection.commit()
     return True
+
+
 ##########################################
 
 #############################CHANGE RANDOM STATUS
@@ -237,6 +277,8 @@ def change_random_status(name, stts):
         sql = "DELETE FROM `random` WHERE uid = " + str(name_uid)
         mycursor.execute(sql)
         connection.commit()
+
+
 ##########################################
 
 #############################RANDOM MATCHING
@@ -248,7 +290,7 @@ def random_uid(name):
     mycursor.execute(sql)
     lst = mycursor.fetchall()[0]
     cnt = 0
-    while True :
+    while True:
         rnd_uid = random.randint(1, lst)
         sql = "SELECT uid FROM `random` WHERE uid = " + str(rnd_uid)
         mycursor.execute(sql)
@@ -258,19 +300,40 @@ def random_uid(name):
         cnt += 1
         if cnt == 100000:
             return -1
+
+
 ############################################
 
 #############################CHECK HUMAN ANSWER
-def check_human_answer (ans, res):
+def check_human_answer(ans, res):
     if ans == res:
         return True
     return False
+
+
 ###############################################
 
 #############################HUMAN VERIFY
-def human_test (g1 = -1, g2 = -1, g3 = -1):
-    hmn_ver_ques = ["Which of the following is an animal?", "Which one is prime?", "Which one is biggest?", "Which one is smallest?", "What is the result of 1/2 + 1/4 + 1/8 + 1/16 + ... ?", "What is the result of 1/3 + 1/9 + 1/27 + 1/81 + ... ?", "What is the biggest 5 digit number?"]
-    hmn_ver_ans = [["1. Dog    2. Book    3. Chair    4. iPhone   5. Pasta", "1. Kitchen    2. Airpod    3. Chicken    4. Door    5. CPU", "1. Banana    2. Video    3. Google    4. Lion    5. Pen"], ["1. 16    2. 13    3. 18    4. 4    5. 20", "1. 10    2. 555555577777    3. 98    4. 10000    5. 6", "1. 11    2. 21    3. 24    4. 12    5. 90"], ["1. 51    2. 1    3. -100    4. 51    5. 40", "1. 80    2. 21    3. 14    4. 82    5. 20", "1. 0    2. -123    3. 1    4. -2    5. -85"], ["1. 16    2. -13    3. 18    4. -4    5. 20", "1. 11    2. 587    3. 322    4. 12    5. 0", "1. -1000    2. 1    3. 2    4. 3    5. 4"], ["1. 3/4    2. 2/3    3. 1    4. 4/5    5. 2", "1. 2/5    2. 1    3. 2    4. 13/16    5. 7/8", "1. 5/8    2. 33/34    3. 1/2    4. 1    5. 2"], ["1. 1/2    2. 2/3    3. 1    4. 4/5    5. 2", "1. 5/8    2. 1/2    3. 8/9    4. 1    5. 2", "1. 2/9    2. 1/81    3. 1/2    4. 1    5. 2/3"], ["1. -99990    2. 99    3. 9999    4. 100000    5. none", "1. 99    2. 99999    3. 999    4. 100000    5. none", "1. 9234    2. -10000    3. 12345    4. 99999    5. 9090"]]
+def human_test(g1=-1, g2=-1, g3=-1):
+    hmn_ver_ques = ["Which of the following is an animal?", "Which one is prime?", "Which one is biggest?",
+                    "Which one is smallest?", "What is the result of 1/2 + 1/4 + 1/8 + 1/16 + ... ?",
+                    "What is the result of 1/3 + 1/9 + 1/27 + 1/81 + ... ?", "What is the biggest 5 digit number?"]
+    hmn_ver_ans = [["1. Dog    2. Book    3. Chair    4. iPhone   5. Pasta",
+                    "1. Kitchen    2. Airpod    3. Chicken    4. Door    5. CPU",
+                    "1. Banana    2. Video    3. Google    4. Lion    5. Pen"],
+                   ["1. 16    2. 13    3. 18    4. 4    5. 20", "1. 10    2. 555555577777    3. 98    4. 10000    5. 6",
+                    "1. 11    2. 21    3. 24    4. 12    5. 90"],
+                   ["1. 51    2. 1    3. -100    4. 51    5. 40", "1. 80    2. 21    3. 14    4. 82    5. 20",
+                    "1. 0    2. -123    3. 1    4. -2    5. -85"],
+                   ["1. 16    2. -13    3. 18    4. -4    5. 20", "1. 11    2. 587    3. 322    4. 12    5. 0",
+                    "1. -1000    2. 1    3. 2    4. 3    5. 4"],
+                   ["1. 3/4    2. 2/3    3. 1    4. 4/5    5. 2", "1. 2/5    2. 1    3. 2    4. 13/16    5. 7/8",
+                    "1. 5/8    2. 33/34    3. 1/2    4. 1    5. 2"],
+                   ["1. 1/2    2. 2/3    3. 1    4. 4/5    5. 2", "1. 5/8    2. 1/2    3. 8/9    4. 1    5. 2",
+                    "1. 2/9    2. 1/81    3. 1/2    4. 1    5. 2/3"],
+                   ["1. -99990    2. 99    3. 9999    4. 100000    5. none",
+                    "1. 99    2. 99999    3. 999    4. 100000    5. none",
+                    "1. 9234    2. -10000    3. 12345    4. 99999    5. 9090"]]
     hmn_ver_res = [[1, 3, 4], [2, 2, 1], [5, 4, 3], [2, 5, 1], [3, 2, 4], [1, 2, 3], [5, 2, 4]]
     qst = -1
     while g1 == qst and g2 == qst and g3 == qst:
